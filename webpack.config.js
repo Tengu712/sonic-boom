07523233f5e4907e47a51e4182100d908d3db04a
Tengu712 <genreihoutengu@gmail.com>
2023-11-16 13:03:38 +0900

    initialize typescript project

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    path: path.join(__dirname, 'pages/js'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [{
+      test: /\.ts$/,
+      use: 'ts-loader'
+    }]
+  },
+  resolve: {
+    modules: ["node_modules"],
+    extensions: ['.ts', '.js']
+  }
+};
